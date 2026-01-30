9145707e27f9e7c1da424cbddb5fa970f9aab328
kundan kumar <kundankumar02003@gmail.com>
2026-01-30 20:40:15 +0530

    Initial working Spring Boot setup

---
+++ src/main/java/com/kdn/kdnelectrical/KdnElectricalApplication.java
@@ -0,0 +1,13 @@
+package com.kdn.kdnelectrical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdnElectricalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdnElectricalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kdn/kdnelectrical/KdnElectricalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdn.kdnelectrical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdnElectricalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
